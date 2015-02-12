263184eec7a79ff2b85519fb9fc7fa06338abfd0
Martin Holm Cservenka <mhcservenka@gmail.com>
2015-02-12 12:12:05 +0100

    Fixed distance and misc bugs in i.3.1

--- src/assignment1/i_3_1.py
+++ src/assignment1/i_3_1.py
@@ -3,37 +3,47 @@ import math
 import numpy as np
 import operator
 import collections
+import copy
 
 def readFile(filename):
+	#Interpret first two columns as floats, last column as int
 	toNum = lambda (a, b) : int(b) if a == 2 else float(b)
+
+	#List of lists, each containing 2 floats and 1 int
 	return [map(toNum, enumerate(line.strip().split(' '))) for line in open(filename)]
 
+#Get euclidean distance between two data points
 def euclideanMetric(a, b, length):
-	distance = 0
-	for i in xrange(length):
-		distance += (a[i] - b[i]) ** 2
-	return math.sqrt(distance)
+	#Length is in mm
+	x1 = (a[0] - b[0]) ** 2
+	#Width is in cm (so mult by 10)
+	x2 = ((a[1] * 10) - (b[1] * 10)) ** 2
+	return math.sqrt(x1 + x2) 
 
 def getNeighbors(train, new, k):
-	train.sort(key = lambda e: euclideanMetric(new, e, len(new)-1))
-	return [train[i] for i in xrange(k)]
+	#Get first k elements of data set sorted ascendingly by distance from new data point
+	sortPred = lambda e: euclideanMetric(new, e, len(new)-1)
+	return sorted(train, key = sortPred)[:k]
 
+#Get most common type amongst neighbors (as integer)
 def getMajority(neighbors):
 	return collections.Counter(map(lambda nb : nb[-1], neighbors)).most_common(1)[0][0]
 
-def getAccuracy(test, results):
-	correct = []
-	for (t, r) in zip(test, results):
-		if t[-1] is r:
-			correct.append(1)
-		else:
-			correct.append(0)
-	return 1 - np.mean(correct)
+#Get list of species (as int) from a data set
+def getSpecies(test):
+	return [e[-1] for e in test]
+
+#Get 1 minus the mean of the 0-1 loss for each data entry
+def getAccuracy(actual, predicted):
+	loss = lambda (p, r): 0 if p is r else 1
+	return 1 - np.mean(map(loss, zip(actual, predicted)))
 
 if __name__ == '__main__':
-	k = 5
-	results = []
 	train = readFile(os.path.dirname(__file__) + '/../../data/IrisTrain2014.dt')
 	test = readFile(os.path.dirname(__file__) + '/../../data/IrisTest2014.dt')
 
-	print getAccuracy(train, [getMajority(getNeighbors(train, e, k)) for e in train])
+	for i in [1, 3, 5]:
+		print "Training set K = %d Accuracy = %s" % (i, getAccuracy(getSpecies(train), [getMajority(getNeighbors(train, e, i)) for e in train]))
+	print ""
+	for i in [1, 3, 5]:
+		print "Test set K = %d Accuracy = %s" % (i, getAccuracy(getSpecies(test), [getMajority(getNeighbors(train, e, i)) for e in test]))
