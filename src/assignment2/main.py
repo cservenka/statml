3c469e4be306d8b6971b387f9ba2b1f457e7a3b9
cservenka <mhcservenka@gmail.com>
2015-03-03 23:42:00 +0100

    Updated main file

--- src/assignment2/main.py
+++ src/assignment2/main.py
@@ -12,4 +12,14 @@ if __name__ == '__main__':
     
     print 'Running ' + ii_1_2.__name__
     ii_1_2.run()
+
+    continuePrompt()
+    
+    print 'Running ' + ii_2_1.__name__
+    ii_2_1.run()
+    
+    continuePrompt()
+    
+    print 'Running ' + ii_2_2.__name__
+    ii_2_2.run()
     
