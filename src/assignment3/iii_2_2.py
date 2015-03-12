ad30526aac7a53e935ed24c3c3e7b55139228f31
Tue Haulund <tue.haulund@gmail.com>
2015-03-12 15:34:03 +0100

    Added hyperparameter grid

--- src/assignment3/iii_2_2.py
+++ src/assignment3/iii_2_2.py
@@ -1,4 +1,6 @@
 import os
+import itertools
+
 import numpy as np
 
 from sklearn import svm
@@ -8,6 +10,12 @@ def readFile(filename):
 	lines_rotated = np.rot90(lines, 3)
 	return (np.rot90([map(float, line) for line in lines_rotated[:-1]]), map(int, lines_rotated[-1])[::-1])
 
-(data, result) = readFile(os.path.dirname(__file__) + '/../../data/parkinsonsTrainStatML.dt')
-print data
-print result
+(train_data, train_result) = readFile(os.path.dirname(__file__) + '/../../data/parkinsonsTrainStatML.dt')
+
+C_range = [0.1, 1, 10, 100, 1000]
+gamma_range = [0.001, 0.01, 0.1, 1, 10]
+hyperparameter_grid = [pair for pair in itertools.product(C_range, gamma_range)]
+
+#classifier = svm.SVC(C = 1.0, kernel = 'rbf')
+#classifier.gamma = 1.0
+#classifier.fit(data, result)
