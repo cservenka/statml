392e5c345a05abf6b7d9e2e5717bc56dc33a57d9
Tue Haulund <tue.haulund@gmail.com>
2015-03-12 15:55:30 +0100

    Added n-fold cross validation skeleton

--- src/assignment3/iii_2_2.py
+++ src/assignment3/iii_2_2.py
@@ -10,11 +10,35 @@ def readFile(filename):
 	lines_rotated = np.rot90(lines, 3)
 	return (np.rot90([map(float, line) for line in lines_rotated[:-1]]), map(int, lines_rotated[-1])[::-1])
 
+# Split lst into chunks of size n.
+def splitList(lst, n):
+    n = max(1, n)
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+def nFoldCrossValidation(n, data, result):
+    accuracy = []
+    groups = splitList(data, len(data) / n)
+    for i in range(n):
+        rest = reduce(operator.add, groups[0:i]+groups[i+1:len(groups)])
+        skip = groups[i]
+        accuracy.append(4) #TODO
+    return np.mean(accuracy)
+
+def hyperparameterSelection(data, result):
+	C_range = [0.1, 1, 10, 100, 1000]
+	gamma_range = [0.001, 0.01, 0.1, 1, 10]
+	hyperparameter_grid = [pair for pair in itertools.product(C_range, gamma_range)]
+
+	results = []
+	for (gamma, C) in hyperparameter_grid:
+		result = nFoldCrossValidation(5, data, result)
+		results.append((result, gamma, C))
+
+	return max(results, key = operator.itemgetter(0))[0]
+
 (train_data, train_result) = readFile(os.path.dirname(__file__) + '/../../data/parkinsonsTrainStatML.dt')
 
-C_range = [0.1, 1, 10, 100, 1000]
-gamma_range = [0.001, 0.01, 0.1, 1, 10]
-hyperparameter_grid = [pair for pair in itertools.product(C_range, gamma_range)]
+
 
 #classifier = svm.SVC(C = 1.0, kernel = 'rbf')
 #classifier.gamma = 1.0
