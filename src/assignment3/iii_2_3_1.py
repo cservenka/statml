0c44ca44d1cd3e593f0e4de572e79de8f08f1676
Tue Haulund <tue.haulund@gmail.com>
2015-03-17 22:22:14 +0100

    Finished III.2.3.1

--- src/assignment3/iii_2_3_1.py
+++ src/assignment3/iii_2_3_1.py
@@ -2,30 +2,23 @@ import os
 from sklearn import svm
 from iii_2_1 import normalize, readFile
 
-C = 1
-gamma = 0.01
-test = readFile(os.path.dirname(__file__) + '/../../data/parkinsonsTestStatML.dt')
-
-classifier = svm.SVC(C = C, kernel = 'rbf')
-classifier.gamma = gamma
-
-result_test = [a[-1] for a in test]
-data_test = [a[:-1] for a in test]
-
-result = classifier.fit(data_test, result_test)
-
-coef = result.dual_coef_[0].tolist()
-
-n_bound = coef.count(C) + coef.count(-C)
-n_free = len(coef) - n_bound
-
-print "bounded: %d \t free: %d" % (n_bound, n_free)
-
-
 def run():
-	print "Hello, world"
+	test = readFile(os.path.dirname(__file__) + '/../../data/parkinsonsTestStatML.dt')
+	gamma = 0.01
+	result_test = [a[-1] for a in test]
+	data_test = [a[:-1] for a in test]
+
+	for C in [0.1, 1, 10, 100, 1000]:
+		classifier = svm.SVC(C = C, kernel = 'rbf')
+		classifier.gamma = gamma
+
+		result = classifier.fit(data_test, result_test)
+		
+		coef = result.dual_coef_[0].tolist()
+		n_bound = coef.count(C) + coef.count(-C)
+		n_free = len(coef) - n_bound
+
+		print "C = %.1f \t bounded support vectors: %d \t free support vectors: %d" % (C, n_bound, n_free)
 
 if __name__ == '__main__':
     run()
-
-
